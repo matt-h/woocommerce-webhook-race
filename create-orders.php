61ce7bf2ab15a051ec747fec905dbe5c03e4cfa8
Matt Harrison <matt@hallme.com>
2020-07-16 17:14:52 -0400

    break create order out into another script

--- create-orders.php
+++ create-orders.php
@@ -8,20 +8,6 @@ require __DIR__ . '/wordpress/wp-config.php';
 // Initialize WordPress.
 wp();
 
-
-// Load WooCommerce Test Helpers.
-$woo_commerce_test_dir = __DIR__ . '/woocommerce/tests/legacy';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-product.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-coupon.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-fee.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-shipping.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-customer.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-order.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-shipping-zones.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-payment-token.php';
-require_once $woo_commerce_test_dir . '/framework/helpers/class-wc-helper-settings.php';
-
-
 // Create WebHook.
 $webhook = new WC_Webhook();
 $webhook->set_props(
@@ -38,16 +24,8 @@ $webhook->set_props(
 $webhook->save();
 wc_load_webhooks( 'active' );
 
-$i = 0;
 while ( ! file_exists( __DIR__ . '/pending.log' ) ) {
-	// Create an order for testing.
-	$wc_order = WC_Helper_Order::create_order();
-
-	// usleep( 100 ); // Add a short sleep here if you can't get it to reproduce the bug.
-
-	// Mark payment as complete which updates status to processing.
-	$wc_order->payment_complete();
-	$i++;
+	`php create-order.php`;
 }
 
 echo "Found webhook that sent pending status. Check pending.log\n";
